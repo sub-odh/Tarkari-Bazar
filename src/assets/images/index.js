470e35747ed1ae9f40644cbf5e135679ba64d1e5
sub-odh <aykahsubodh@gmail.com>
2024-08-26 14:11:53 +0545

    Added: Footer Section

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,7 +3,7 @@ import banner from "./banner.svg";
 import card_1 from "./card1.svg";
 import card_2 from "./card2.svg";
 import card_3 from "./card3.svg";
-import videoImg from "./video.svg";
+import videoImg from "./Video.svg";
 
 import codyImg from "./team/cody.svg";
 import janeImg from "./team/jane.svg";
